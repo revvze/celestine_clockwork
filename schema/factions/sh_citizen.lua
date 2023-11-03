52e66c9a8fb42a005c2c7b950b41e4b00688284a
Larry <elijah.j.staley@gmail.com>
2023-11-02 22:18:21 -0600

    citizen models

--- schema/factions/sh_citizen.lua
+++ schema/factions/sh_citizen.lua
@@ -10,7 +10,28 @@ FACTION.material = "hl2rp2/factions/citizen";
 FACTION.startingInv = {
 	["cw_suitcase"] = 1
 };
-
+FACTION.models = {
+	female = {
+		"models/humans/pandafishizens/female_01.mdl",
+		"models/humans/pandafishizens/female_02.mdl",
+		"models/humans/pandafishizens/female_03.mdl",
+		"models/humans/pandafishizens/female_04.mdl",
+		"models/humans/pandafishizens/female_06.mdl",
+		"models/humans/pandafishizens/female_07.mdl"
+},
+	male = {
+		"models/humans/pandafishizens/male_01.mdl",
+		"models/humans/pandafishizens/male_02.mdl",
+		"models/humans/pandafishizens/male_03.mdl",
+		"models/humans/pandafishizens/male_04.mdl",
+		"models/humans/pandafishizens/male_05.mdl",
+		"models/humans/pandafishizens/male_06.mdl",
+		"models/humans/pandafishizens/male_07.mdl",
+		"models/humans/pandafishizens/male_08.mdl",
+		"models/humans/pandafishizens/male_09.mdl",
+		"models/humans/pandafishizens/male_10.mdl"
+}
+};
 
 -- Called when a player is transferred to the faction.
 function FACTION:OnTransferred(player, faction, name)
