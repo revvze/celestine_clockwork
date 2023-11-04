7b2c6dedc6f8d0874548e0fae8de7fe0d26d5e84
RVVZ <elijah.j.staley@gmail.com>
2023-11-04 17:22:17 -0600

    mleb

--- plugins/talkingdisplay/plugin/sh_plugin.lua
+++
@@ -1,9 +0,0 @@
-local PLUGIN = PLUGIN;
-
-Clockwork.kernel:IncludePrefixed("cl_hooks.lua");
-Clockwork.kernel:IncludePrefixed("sv_plugin.lua");
-
--- Called when the Clockwork shared variables are added.
-function PLUGIN:ClockworkAddSharedVars(globalVars, playerVars)
-	playerVars:String("SpeechText");
-end;
